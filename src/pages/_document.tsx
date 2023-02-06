987f3d569edbeba730bd82d9e282590b8725b287
Joshua Nguyen <joshnguyen@Joshuas-Laptop.localdomain>
2023-02-05 16:57:32 -0800

    restructured project files. implemented codegen cli, tanstack query, and thirdweb.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import NavBar from './api/components/NavBar'
-import { LivepeerConfig } from '@livepeer/react'
-import { livepeerClient } from '@/livepeer-api'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className='bg-background text-white text-inter font-regular' >
-        <LivepeerConfig client={livepeerClient}>
-          <NavBar></NavBar>
           <Main />
-
+          hello
           <NextScript />
-        </LivepeerConfig>
       </body>
     </Html>
   )
