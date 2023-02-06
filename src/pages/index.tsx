447c4665e0c718a6ffbb7f3f9184cf39d816e137
jn-han <joshuahnguyen@gmail.com>
2023-02-05 21:58:32 -0800

    switched tailwind.config.js to src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,11 @@
+import NavBar from '../components/navigation/NavBar'
 
-import { ConnectWallet, useAddress} from '@thirdweb-dev/react';
-import { PublicationSortCriteria, useExplorePublicationsQuery } from '../graphql/generated';
-import useLogin from '../auth/useLogin'
 
 export default function Home() {
 
-  const address = useAddress();
-  const { mutate: requestLogin } = useLogin();
-
-  if(!address) {
-    return (<ConnectWallet/>)
-  }
-
-
-
   return (
-    <div>
-      <button onClick={ () => requestLogin()}>Login</button>
+    <div className=''>
+      <NavBar />
     </div>
   )
 }
